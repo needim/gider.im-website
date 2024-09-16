1e25f5c0b5af17375f504a497dd9488b0f7d4588
Emre Kayık <54414075+emrekayik@users.noreply.github.com>
2024-09-16 14:55:39 +0000

    simple spacing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@/components/section/hero";
 
 export default function Home() {
 	return (
-		<div className="max-w-7xl mx-auto">
+		<div className="max-w-7xl mx-auto mb-8">
 			<Hero />
 			<CorporateSponsors />
 		</div>
--- components/section/hero.tsx
+++ components/section/hero.tsx
@@ -5,17 +5,17 @@ import Image from "next/image";
 export function Hero() {
 	return (
 		<>
-			<div className="mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 lg:items-start pt-8 xl:px-0">
+			<div className="mx-auto flex flex-col lg:flex-row items-start justify-center gap-16 lg:gap-20 lg:items-start pt-8 xl:px-0">
 				<div className="flex flex-col gap-4 lg:gap-8 md:gap-8 items-center justify-center text-center lg:text-left lg:items-start lg:px-6 xl:px-0">
 					<h1 className="font-extrabold text-3xl md:text-4xl lg:text-5xl xl:mt-4 tracking-tight flex flex-col gap-3 items-center lg:items-start text-balance">
 						Privacy focused income & expense tracking app.
 					</h1>
 					<div className="flex flex-row md:max-w-lg justify-center lg:justify-start flex-wrap gap-x-2 gap-y-1">
 						{features.map((feature) => (
-							<div key={feature.name} className="flex gap-1 items-center">
-								{feature.icon}
-								<div className="text-lg text-balance">{feature.name}</div>
-							</div>
+								<div key={feature.name} className="flex gap-1 items-center">
+									{feature.icon}
+									<div className="text-lg text-balance">{feature.name}</div>
+								</div>
 						))}
 					</div>
 					<div className="">
